e8a3bf09de8e0ae7729730fb5dcb2c22a2992bdc
Ezejneli <emir_2201@live.com>
2024-12-17 11:42:42 +0000

    updated api

--- app.js
+++ app.js
@@ -9,13 +9,6 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
 app.use('/api', apiRouter);
 app.use('/api/comments', commentsRouter);
 
@@ -24,10 +17,13 @@ app.all('/*', (req, res) => {
 });
 
 app.use((err, req, res, next) => {
+  console.error('Error:', err);
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
   } else if (err.code === '23503') {
-    res.status(404).send({ msg: 'Not Found' });  } else if (err.code === '22P02' || err.code === '23502') {    res.status(400).send({ msg: 'Bad Request' });
+    res.status(404).send({ msg: 'Not Found' });
+  } else if (err.code === '22P02' || err.code === '23502') {
+    res.status(400).send({ msg: 'Bad Request' });
   } else {
     res.status(500).send({ msg: 'Internal Server Error' });
   }
--- models/topicsModel.js
+++
@@ -1,7 +0,0 @@
-const topics = process.env.NODE_ENV === 'test'
-  ? require('../db/data/test-data/topics')
-  : require('../db/data/development-data/topics');
-
-exports.fetchTopics = () => {
-  return topics;
-};
