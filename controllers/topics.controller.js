d7d57e6a47cce0a935823ada7f343faff827b797
Ezejneli <emir_2201@live.com>
2024-12-17 11:43:25 +0000

    updated api

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,8 +1,6 @@
 const { fetchTopics } = require('../models/topics.model');
 
 exports.getTopics = (req, res, next) => {
-  console.log('Received request to get topics');
-  console.log('Starting to fetch topics from the database');
   fetchTopics()
     .then((topics) => {
       res.status(200).send({ topics });
